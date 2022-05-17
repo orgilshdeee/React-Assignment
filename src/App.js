001a541fdecddbdad59633010c1192584989d676
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-05-17 15:12:55 +0800

    init

--- src/App.js
+++ src/App.js
@@ -5,18 +5,40 @@ import Contents from "./components/Contents";
 import Content from "./components/Content";
 import { Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import WebFont from "webfontloader";
+import { useTheme } from "./components/useTheme";
+import { GlobalStyles } from "./components/GlobalStyles";
+
+const Container = styled.div`
+  margin: 5px auto 5px auto;
+`;
 
 function App() {
   const [langs, setLangs] = useState({});
   const [titles, setTitles] = useState();
+
+  const { theme, themeLoaded, getFonts } = useTheme();
+  const [selectedTheme, setSelectedTheme] = useState(theme);
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: getFonts(),
+      },
+    });
+  });
+
   useEffect(() => {
     fetch("data/ProgLangs.json")
       .then((e) => e.json())
       .then((res) => setLangs(res));
   }, []);
+
   useEffect(() => {
     setTitles(Object.keys(langs));
   }, [langs]);
+
   return (
     <div
       style={{
@@ -26,31 +48,36 @@ function App() {
           "linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1))",
       }}
     >
-      <Header data={langs} />
-      <div className="body">
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          {titles &&
-            titles.map((title, i) => (
-              <Route
-                path={`/${title}`}
-                key={i}
-                element={<Contents data={Object.keys(langs[`${title}`])} />}
-              >
-                {Object.keys(langs[`${title}`]).map((innerTitle, i) => {
-                  return (
-                    <Route
-                      path={innerTitle}
-                      key={i}
-                      element={<Content data={langs[title][innerTitle]} />}
-                    ></Route>
-                  );
-                })}
-              </Route>
-            ))}
-        </Routes>
-      </div>
-      <Footer />
+      {themeLoaded && (
+        <ThemeProvider theme={selectedTheme}>
+          <GlobalStyles />
+          <Header data={langs} />
+          <div className="body" style={{ fontFamily: selectedTheme.font }}>
+            <Routes>
+              <Route path="/" element={<Main />}></Route>
+              {titles &&
+                titles.map((title, i) => (
+                  <Route
+                    path={`/${title}`}
+                    key={i}
+                    element={<Contents data={Object.keys(langs[`${title}`])} />}
+                  >
+                    {Object.keys(langs[`${title}`]).map((innerTitle, i) => {
+                      return (
+                        <Route
+                          path={innerTitle}
+                          key={i}
+                          element={<Content data={langs[title][innerTitle]} />}
+                        ></Route>
+                      );
+                    })}
+                  </Route>
+                ))}
+            </Routes>
+          </div>
+          <Footer />
+        </ThemeProvider>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import * as themes from "./components/schema.json";
+import { setToLS } from "./components/storage";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+const Index = () => {
+  setToLS("all-themes", themes.default);
+  return (
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
