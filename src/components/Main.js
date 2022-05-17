fc6dc07340bdd9645a7daacd97df8c842ec5103f
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-05-17 10:20:23 +0800

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,16 @@
 function Main() {
   return (
     <>
-      <div style={{ textAlign:"center",  height: "100vh", backgroundColor:"azure"}}>
+      <div
+        style={{
+          textAlign: "center",
+          height: "100vh",
+          backgroundImage:
+            "linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1))",
+        }}
+      >
         <p className="pt-5">React Router Assignment</p>
-        </div>
+      </div>
     </>
   );
 }
